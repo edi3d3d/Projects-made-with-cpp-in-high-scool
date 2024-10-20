aaa57beb2cf14290ace61c3999b1059a73672734
edi3d3d <74777571+edi3d3d@users.noreply.github.com>
2024-10-20 16:19:13 +0300

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -5,6 +5,7 @@
 #include <thread>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 using namespace std;
 using namespace std::chrono;
 ofstream g("debugging.txt");
@@ -12,37 +13,36 @@ int score=0;
 int high_score=0;
 bool gameover = false;
 char direction='s';
-int length = 15;            //change length and height (max=~30)
-int height = 15;
+int length = 10;            //change length and height (max=~30)
+int height = 10;            //cu cat este mai mare cu atat creste posibilitatea de stuttering
 int a[30][30];
 int tail=0;
-struct{
+struct{                     //coordonate i si j din matrice
     int ii=0;
     int jj=0;
 } fruit, snake[900];
-int abso(int n)
+int abso(int n)             //absolut sau modulul matematic
 {
     if(n<0) return -n;
     return n;
 }
-void sleep(int microsec)
+void sleep(int microsec)    //pune pauza, unitatea de timp este microsecunda
 {
     std::this_thread::sleep_for(std::chrono::microseconds(microsec));
 }
-void gen_fruit()
+void gen_fruit()            //generarea fructului
 {
     int v[1000];
     int k=0;
     for(int i=0;i<=height;i++)
         for(int j=0;j<=length;j++)
-            if(a[i][j]==0) {v[k]=90000+i*100+j; k++;}
-    int poz=rand()%k;
-    fruit={v[poz]/100%100, v[poz]%100};
-    //g<<"fruit:"<<fruit.ii<<" "<<fruit.jj<<" "<<poz<<endl;
+            if(a[i][j]==0) {v[k]=90000+i*100+j; k++;}           //se noteaza fiecare pozitie libera
+    int poz=rand()%k;                                           //folosind un generator de numere intamplator si se alege o pozitie noua a fructului
+    fruit={v[poz]/100%100, v[poz]%100};                         //actualizarea fructului cu pozitia noua
 }
-void setup()
+void setup()                //generarea tablei goale, a zidului inconjurator, a sarpelui si a fructului
 {
-    srand(time(0));
+    srand(time(0));         //salvarea timpului initial pentru generatorul de numere a fructului
     for(int i=0;i<=length+1;i++)
     {
         a[0][i]=1;
@@ -58,110 +58,105 @@ void setup()
     a[fruit.ii][fruit.jj]=3;
     a[snake[0].ii][snake[0].jj]=2;
 }
-void draw()
+void draw()                 //afisare. Fiecare valoare este salvata intr-un vector de caractere, inclusiv sfarsitul de linie si este afisat la final pentru a elimina stuttering-ul
 {
+    system("cls");
+    char total[10000]="";
+    char aux[100]="\n";
     for(int i=0;i<=height+1;i++)
     {
+        char s[100]="";
         for(int j=0;j<=length+1;j++)
         {
-            if(a[i][j]==0) cout<<"  ";          //nothing
-            else if(a[i][j]==1) cout<<"# ";     //wall
-            else if(a[i][j]==2) cout<<"* ";     //snake
-            else if(a[i][j]==3) cout<<"% ";     //fruit
+            if(a[i][j]==0) s[j*2]=' ';          //nothing
+            else if(a[i][j]==1) s[j*2]='#';     //wall
+            else if(a[i][j]==2) s[j*2]='*';     //snake
+            else if(a[i][j]==3) s[j*2]='%';     //fruit
+            s[j*2+1]=' ';
         }
-        cout<<endl;
+        strcat(total,s);
+        strcat(total,aux);
     }
-    cout<<"Score:"<<tail*10;
-    cout<<endl<<endl<<endl<<endl<<endl<<endl<<endl;
+    cout<<total<<"Score:"<<tail*10<<'\n';
 }
 void move()
 {
     
-    if(snake[tail].ii!=0 && snake[tail].jj!=0)
+    if(snake[tail].ii!=0 && snake[tail].jj!=0)  //eliminarea "cozii" sarpelui pentru a putea muta "corpul" in fata
     {
         a[snake[tail].ii][snake[tail].jj]=0;
     }
     
-    for(int i=tail;i>0;i--)
+    for(int i=tail;i>0;i--)                     //miscarea "corpului" sarpelui
         snake[i]=snake[i-1];
     
-    switch(direction)
+    switch(direction)                           //schimbarea directiei si miscarea "capului" sarpelui
     {
         case 'w':
-            //if(snake[0].ii-1==2) gameover=true;
             snake[0].ii--;
             break;
         case 'a':
-            //if(snake[0].jj-1==2) gameover=true;
             snake[0].jj--;
             break;
         case 's':
-            //if(snake[0].ii+1==2) gameover=true;
             snake[0].ii++;
             break;
         case 'd':
-            //if(snake[0].jj+1==2) gameover=true;
             snake[0].jj++;
             break;
     }
-    //a[snake[0].ii][snake[0].jj]=2;
 
-    if(snake[0].ii==0 || snake[0].ii==height+1)
+    if(snake[0].ii==0 || snake[0].ii==height+1)         //detectarea de zid si mutarea sarpelui la zidul opus
     {
         a[snake[0].ii][snake[0].jj]=1;
         snake[0].ii=abso(height-snake[0].ii);
     }
-    else if(snake[0].jj==0 || snake[0].jj==length+1)
+    else if(snake[0].jj==0 || snake[0].jj==length+1)    //detectarea de zid si mutarea sarpelui la zidul opus
     {
         a[snake[0].ii][snake[0].jj]=1;
         snake[0].jj=abso(length-snake[0].jj);
     }
-    a[snake[0].ii][snake[0].jj]=2;
-    if(snake[0].ii==fruit.ii && snake[0].jj==fruit.jj)
+    a[snake[0].ii][snake[0].jj]=2;                      //desenarea "capului" nou al sarpelui
+    if(snake[0].ii==fruit.ii && snake[0].jj==fruit.jj)  //atunci cand "capul" sarpelui mananca fruct:
     {
-        a[fruit.ii][fruit.jj]=2;
-        tail++;
-        gen_fruit();
-        a[fruit.ii][fruit.jj]=3;
+        a[fruit.ii][fruit.jj]=2;                        //fructul devine sarpele
+        tail++;                                         //coada creste iar actualizarea o sa se intample la prima schimbare a sarpelui
+        gen_fruit();                                    //generarea unui fruct nou
+        a[fruit.ii][fruit.jj]=3;                        //pozitia fructului actualizata
     }
-    for(int i=1;i<=tail;i++)
+    for(int i=1;i<=tail;i++)                            //verificare sfarsit joc (daca "capul" sarpelui atinge "corpul")
     {
         if(snake[i].ii==snake[0].ii && snake[i].jj==snake[0].jj) gameover=true;
     }
     
 }
-void input()
+void input()                //citirea directiei, pauza sau opririi programului
 {
     char aux=direction;
     for(int value=1;value<=10; value++)
     {
-        if(_kbhit())
+        if(_kbhit())    //detectare input de la tastatura
         {
-            char aux1=_getch();
-            if((aux1=='w' && aux!='s') || (aux1=='a' && aux!='d') || (aux1=='s' && aux!='w') || (aux1=='d' && aux!='a') || aux1=='p' || aux1=='8') direction=aux1;
+            char aux1=_getch(); //in caz de detectare iti returneaza caracterul tastat
+            if((aux1=='w' && aux!='s') || (aux1=='a' && aux!='d') || (aux1=='s' && aux!='w') || (aux1=='d' && aux!='a') || aux1=='p' || aux1=='8') direction=aux1;  //verificare pentru schimbare de directie, pauza sau "opreste jocul"
         }
         sleep(1000);
     }
 }
 int main()
 {
-    setup();
-    
-    while(!gameover)
+    setup();                //generare
+
+    while(!gameover)        //se repeta pana cand jocul se termina
     {
-        //auto begin = std::chrono::high_resolution_clock::now();
+        draw();             //se deseneaza sarpele si zidul
 
-        draw();
         do{
-            input();
-            if(direction=='8') gameover=true;
-        }while(direction=='p');
-        move();
+            input();        //se citeste input
+            if(direction=='8') gameover=true;               //sfarsit de joc
+        }while(direction=='p');                             //se pune pauza
 
-        //auto end = std::chrono::high_resolution_clock::now();
-        //auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-        //g<<elapsed.count()*1e-9<<" seconds"<<endl;
+        move();                                             //actualizarea tablei si a pozitiei sarpelui
     }
-    cout<<"Score:"<<tail*10;
     return 0;
 }
